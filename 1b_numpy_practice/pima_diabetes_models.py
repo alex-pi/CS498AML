6568faad720507ec1fa677aaf214bbfcdedce6b3
alejanpi <alejandro.pimentel_rios@nokia.com>
2022-02-12 19:25:21 -0600

    first commit

---
+++ 1b_numpy_practice/pima_diabetes_models.py
@@ -0,0 +1,15 @@
+#%matplotlib inline
+#%load_ext autoreload
+#%autoreload 2
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#from utils import test_case_checker
+
+df = pd.read_csv('diabetes.csv')
+print(df.head())
+
+
